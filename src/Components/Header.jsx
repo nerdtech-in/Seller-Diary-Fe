cd708f46972c41136c1f90fc45672c766bdd4d01
enryu-maker <akifkhan60067@gmail.com>
2024-02-10 03:07:52 +0530

    cc

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -5,7 +5,7 @@ export default function Header({
 }) {
     return (
         <div
-            className='h-[8%]  mt-3 flex justify-center items-center'
+            className='h-[8%]  pt-3 flex justify-center items-center'
         >
             <p
                 className='text-2xl  tracking-widest font-Raleway  text-gray-900 dark:text-white'
